6cdc7170937a321a48409b2a9e62f27e22662004
tsov09 <tsovinarsafaryan147@gmail.com>
2023-07-04 23:09:20 +0400

    added gen_element

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-//#include <string>
+#include <stdlib.h>
+#include <time.h>
 
 class Array {
 public:
+	Array() {
+		this->size = 10;
+		this->ptr = new int[this->size];
+		this->fill_array();
+	}
 	Array(int size) {
 		this->size = size;
 		this->ptr = new int[this->size];
+		this->fill_array();
 	}
 	~Array() {
 		delete [] this->ptr;
@@ -32,33 +39,31 @@ public:
 		return *this;
 	}
 	int& operator[] (int i){
-		if (i >= this->size) {
-			std::cout << "The number is out of range" << std::endl;
+		if (i >= this->size || i < 0) {
+			std::cout << "The number is out of range" << std::endl << std::endl;
 			exit(0);
 		}
 		else {
 			return this->ptr[i];
 		}
 	}
-
-	//Time operator+(const Time& lhs, const Time& rhs) {
-//	Time temp = lhs;
-//	temp.seconds += rhs.seconds;
-//	temp.minutes += temp.seconds / 60;
-//	temp.seconds %= 60;
-//	temp.minutes += rhs.minutes;
-//	temp.hours += temp.minutes / 60;
-//	temp.minutes %= 60;
-//	temp.hours += rhs.hours;
-//	return temp;
-//}
-
 private:
 	int* ptr;
 	int size;
+	void fill_array() {
+		for (int i = 0; i < this->size; i++) {
+			this->ptr[i] = rand() % 100 + 10;
+		}
+	}
 public:
 	int gen_element(int x) {
-		return x;
+		if (x >= this->size || x < 0) {
+			std::cout << "The number is out of range" << std::endl << std::endl;
+			exit(0);
+		}
+		else {
+			return this->ptr[x];
+		}
 	}
 	void print() {
 		std::cout << "{ ";
@@ -71,23 +76,19 @@ public:
 };
 
 int main() {
-
-	Array arr(10);
-	arr.print();
-	std::cout << arr[18];
+	srand(time(NULL));
+	int a = 15;
+	int b = 17;
+	Array arr;
 
 	std::cout << std::endl;
-
-	Array arr_1(7);
-	arr_1.print();
-	
-	std::cout << std::endl;
-
-	arr_1 = arr;
 	arr.print();
 	std::cout << std::endl;
-	Array arr_2 = arr;
-	arr_2.print();
+	std::cout << std::endl;
+	std::cout << a + 1 << " - " << arr[a] << std::endl;
+	std::cout << b + 1 << " - " << arr.gen_element(b) << std::endl;
+
 
 	return 0;
 }
+ 
